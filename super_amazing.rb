95fe85303f9ecb8360770b3b4cf50b0996be2491
Emily Platzer <emilyplatzer@gmail.com>
2014-03-21 11:31:29 -0600

    Now can add items to the menu via the new item page...
    writing to csv, then redirecting and displaying on the item page
    
    Signed-off-by: Andrew Deitrick <ajdeitrick@gmail.com>

--- super_amazing.rb
+++ super_amazing.rb
@@ -19,3 +19,15 @@ get '/items' do
   end
   erb :items
 end
+
+get '/items/new' do
+  erb :new
+end
+
+post '/items' do
+  name = params[:name]
+  IO.write("./config/menu.csv", "\n#{name},#{IO.readlines("./config/menu.csv").length}", IO.read("./config/menu.csv").length)
+  @items = ItemRepository.new.create_items
+
+  erb :items
+end
