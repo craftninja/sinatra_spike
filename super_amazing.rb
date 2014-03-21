c14db65a60e04e06d66f878b525969e34704f826
Andrew Deitrick <ajdeitrick@gmail.com>
2014-03-21 10:11:42 -0600

    Added functionality to filter menu items in url
    Signed-off-by: Emily Platzer <emilyplatzer@gmail.com>

--- super_amazing.rb
+++ super_amazing.rb
@@ -6,7 +6,24 @@ get '/' do
   erb :index
 end
 
-get '/items/' do
-  @items = ItemRepository.new.create_items
+get '/items' do
+  filter = params[:filter]
+  all_items = ItemRepository.new.create_items
+  @items = []
+  if filter
+    all_items.each do |item|
+      @items << item if item.name.include?(filter)
+    end
+  else
+    @items = all_items
+  end
   erb :items
 end
+
+
+#get '/posts' do
+#  +  # matches "GET /posts?title=foo&author=bar"
+#  +  title = params[:title]
+#  +  author = params[:author]
+#  +  # uses title and author variables; query is optional to the /posts route
+#  +end
