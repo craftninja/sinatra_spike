2e04454baf72ac49487441e0f574ee20548476a3
Emily Platzer <emilyplatzer@gmail.com>
2014-03-21 10:50:29 -0600

    Extracted menu to a CSV file to prepare for adding write to menu functionality
    Signed-off-by: Andrew Deitrick <ajdeitrick@gmail.com>

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,11 +1,16 @@
 class ItemRepository
 
   def create_items
-    item_list = [
-                  Item.new("Sally's delicious Special Spaghetti", 1),
-                  Item.new("Will's delectable Wild Wings", 2),
-                  Item.new("Bob's delightful Buffalo Burgers", 3),
-                ]
+
+    menu_csv = IO.readlines("./config/menu.csv")
+    menu_csv.shift
+
+    item_list = []
+    menu_csv.each do |item|
+      item_array = []
+      item_array = item.split(',')
+      item_list << Item.new(item_array[0],item_array[1])
+    end
     item_list
   end
 
